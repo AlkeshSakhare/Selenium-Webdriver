45323bf03d5d3aff75dde97a00cbdf4ceccfd555
Alkesh <Alkesh@Alkesh-PC>
2018-03-14 23:05:24 +0530

    test

--- Automation/src/BasicStuff/ChromeBrowser.java
+++ Automation/src/BasicStuff/ChromeBrowser.java
@@ -8,7 +8,6 @@ public class ChromeBrowser
     
     public static void main(String[] args)
     {
-	// TODO Auto-generated method stub
 	
 	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
